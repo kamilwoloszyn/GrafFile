7d5e49bd8754e09da1947e691d7a5b8b14ea46c2
= <twalasik97@gmail.com>
2020-01-11 14:08:26 +0100

    Edited FileInfo class

--- src/net/example/cebulasoft/graffile/FileInfo.java
+++ src/net/example/cebulasoft/graffile/FileInfo.java
@@ -1,20 +1,35 @@
 package net.example.cebulasoft.graffile;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Objects;
 
 public class FileInfo {
-   private String path; // paczka
-   private String name; // nazwa pliku razem z rozszerzeniem.
-   private int references; // ilość odwołań
+    private String path; // paczka
+    private String name; // nazwa pliku razem z rozszerzeniem.
+    private HashMap<String, Integer> references; // przechowuje liste plików z których korzysta plik.
 
     public FileInfo(String path, String name) {
         this.path = path;
         this.name = name;
-        this.references = 0;
+        this.references = new HashMap<String, Integer>();
     }
 
     public String getPath() {
         return path;
+    }
+
+    public void addReference(String file){
+        if(references.containsKey(file)){
+            references.put(file, references.get(file) + 1);
+        }
+        else{
+            references.put(file, 1);
+        }
+    }
+
+    public Iterator<Integer> getIteratorToReferences(){
+        return references.values().iterator();
     }
 
     public void setPath(String path) {
@@ -28,17 +43,9 @@ public class FileInfo {
     public void setName(String name) {
         this.name = name;
     }
-
-    public int getReferences() {
-        return references;
-    }
-
-    public void addReference() {
-        this.references++;
-    }
     @Override
     public int hashCode() {
-        return Objects.hash(path, name, references);
+        return Objects.hash(path, name);
     }
 }
 
