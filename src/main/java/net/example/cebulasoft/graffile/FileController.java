002679f81a7229f5a04b2b1fb903c84eaf497358
Kamil Krzyworączka <kakk@ailleron.com>
2020-01-16 18:43:49 +0100

    Reverted FileController to base state

--- src/main/java/net/example/cebulasoft/graffile/FileController.java
+++ src/main/java/net/example/cebulasoft/graffile/FileController.java
@@ -1,10 +1,5 @@
 package net.example.cebulasoft.graffile;
 
-import net.example.cebulasoft.graffile.method.FileParser;
-import net.example.cebulasoft.graffile.method.MethodDependenciesResolver;
-import net.example.cebulasoft.graffile.method.MethodDependencyValidator;
-import net.example.cebulasoft.graffile.method.MethodInfo;
-
 import java.util.List;
 
 public class FileController {
@@ -12,26 +7,20 @@ public class FileController {
 
     public FileController(String catalog) {
         filesCollection = new FilesConnectionInfo();
-        List<String> listFile = FileFinder.getListOfFile(catalog, "java"); // take all file with extension .java and set them all names to list.
+        List<String> listFile = FileFinder.getListOfFile("java", catalog); // take all file with extension .java and set them all names to list.
 
-        //FileInformer.getInfo(listFile, filesCollection); //get information of file and put it in to the container.
-        //FileGrafAdapter graf = new FileGrafAdapter(filesCollection); // make graf
-        //graf.show(); // show graf
-        MethodDependenciesResolver methodDependenciesResolver = new MethodDependenciesResolver(
-                listFile,
-                new MethodDependencyValidator(),
-                new FileParser());
-        List<MethodInfo> dependencies = methodDependenciesResolver.findDependencies();
-        dependencies.forEach(System.out::println);
+        FileInformer.getInfo(listFile, filesCollection); //get information of file and put it in to the container.
+
+        FileGrafAdapter graf = new FileGrafAdapter(filesCollection); // make graf
+        graf.show(); // show graf
 
     }
 
     public static void main(String... args) {
-//        FilesConnectionInfo i= new FilesConnectionInfo();
-//        FileGrafAdapter graf = new FileGrafAdapter(i); // make graf
-//        graf.show(); // show graf
-//        System.out.print("działa?");
-        FileController fileController = new FileController("C:\\Users\\kakk\\IdeaProjects\\wimiip\\GrafFileTest");
+        FilesConnectionInfo i = new FilesConnectionInfo();
+        FileGrafAdapter graf = new FileGrafAdapter(i); // make graf
+        graf.show(); // show graf
+        System.out.print("działa?");
     }
 
 }
